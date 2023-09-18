34050454482d7cee24cbbcd51115c6a4b9e559ed
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-09-18 11:44:54 -0300

    Add: add form popups functionalities

---
+++ src/components/PopupWithForm.jsx
@@ -0,0 +1,26 @@
+import closeIcon from "../images/CloseIconclose.svg";
+
+export default function PopupWithForm(props) {
+  return (
+    <>
+      <div className={`popup ${props.name} ${props.isOpen ? 'popup_active' : ''}`}>
+        <h2 className="popup__title">{props.title}</h2>
+        <button className="popup__close-button" onClick={props.onClose}>
+          <img src={closeIcon} alt="Botão fechar" />
+        </button>
+        <form
+          className={`popup__form ${props.formClass}`}
+          name={props.formName}
+          noValidate
+        >
+          <fieldset className="popup__set">
+            {props.children}
+            <button type="submit" className={`popup__submit ${props.buttonClass}`} >
+              {props.buttonText}
+            </button>
+          </fieldset>
+        </form>
+      </div>
+    </>
+  );
+}
