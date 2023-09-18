f8bb61b852f669b5a12b957283670158555ae1a5
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-09-18 11:44:04 -0300

    Add: add footer functionalities

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+        <p className="copyright">
+          &copy; <span className="footer__year">2021</span> Around the U.S.
+        </p>
+      </footer>
+    );
+  }
+}
