563a11f6d258d1e9995ecd07a146b8ae18909ce3
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-09-26 13:57:14 -0300

    Update: update to default instance

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,7 @@
-import Api from './api.js';
+/* eslint-disable import/no-anonymous-default-export */
+import Api from "./api.js";
 
-const api = new Api(
+export default new Api(
   "https://around.nomoreparties.co/v1/web_ptbr_05",
   "2435b682-2492-4802-b266-a6d24ed22bde"
 );
-
-export default api;
